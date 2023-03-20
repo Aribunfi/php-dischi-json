f00fc8586763e2219f4770ed51997f136263ec9c
Aribunfi <arianna@pabis.de>
2023-03-20 08:59:10 +0100

    tentativo di aggiustamento

--- API/get-discs.php
+++ API/get-discs.php
@@ -1,7 +1,34 @@
 <?php 
 
 $data = file_get_contents(__DIR__ . "../data/discs.json");
-header("Content-Type: application/json");
 
-echo $data;
+$discs = json_decode($data);
+
+$result = null;
+
+if(isset($_GET["title"])) {
+    $searched_title = $_GET["title"];
+  
+    foreach($discs as $disc){
+      if($disc->title === $searched_title) {
+        $result = $disc;
+      }
+    }
+    
+  } else {
+    
+    $result = [];
+    foreach($discs as $disc){
+      $result[] = [
+        "title" => $disc->title,
+        "author" => $disc->author,
+        "poster" => $disc->poster,
+      ];
+    }
+  }
+  
+  header("Content-Type: application/json");
+  echo json_encode($result);
+  
+
 
