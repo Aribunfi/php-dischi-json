b6bf293fac7707b1b8d38a2be8933ba61ebd0570
Aribunfi <arianna@pabis.de>
2023-03-20 17:57:16 +0100

    correzione fatta con Alessandro

--- API/get-discs.php
+++ API/get-discs.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
+$lista = file_get_contents(__DIR__ . '/../data/discs.json');
+$lista_decoded = json_decode($lista);
 
-$data = file_get_contents(__DIR__ . "../data/discs.json");
-header("Content-Type: application/json");
-
-return $data;
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($lista_decoded);
+?>
