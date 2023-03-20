22de1b24ba10decd172bfa6e2cff7fd3702ab20d
Aribunfi <arianna@pabis.de>
2023-03-20 09:02:46 +0100

    per disperazione provo con lavoro Tiziano

--- API/get-discs.php
+++ API/get-discs.php
@@ -1,34 +1,33 @@
 <?php 
 
-$data = file_get_contents(__DIR__ . "../data/discs.json");
+$data = file_get_contents(__DIR__ . "/../data/discs.json");
 
 $discs = json_decode($data);
 
 $result = null;
 
 if(isset($_GET["title"])) {
-    $searched_title = $_GET["title"];
-  
-    foreach($discs as $disc){
-      if($disc->title === $searched_title) {
-        $result = $disc;
-      }
-    }
-    
-  } else {
-    
-    $result = [];
-    foreach($discs as $disc){
-      $result[] = [
-        "title" => $disc->title,
-        "author" => $disc->author,
-        "poster" => $disc->poster,
-      ];
+  // rispondiamo col singolo disco
+  $searched_title = $_GET["title"];
+
+  foreach($discs as $disc){
+    if($disc->title === $searched_title) {
+      $result = $disc;
     }
   }
   
-  header("Content-Type: application/json");
-  echo json_encode($result);
+} else {
+  // rispondiamo con la lista dei dischi
   
+  $result = [];
+  foreach($discs as $disc){
+    $result[] = [
+      "title" => $disc->title,
+      "author" => $disc->author,
+      "poster" => $disc->poster,
+    ];
+  }
+}
 
-
+header("Content-Type: application/json");
+echo json_encode($result);
